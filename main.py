6ccceaf92e5f33abafcb2a91b1e39c4149d86bc0
elixc20 <elicombs2020@gmail.com>
2024-02-01 15:49:20 -0600

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 #Remove pass and complete the code
 def check_character(word, index):
-   pass
+   if 0 < index < len(word):
+      index_character = word[index]
+   if index_character.isalpha():
+      return print("letter")
+   if index_character.isspace():
+      return print("white space")
+   if index_character.isdigit():
+      return print("digit")
+   else:
+      return print("unknown")
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
