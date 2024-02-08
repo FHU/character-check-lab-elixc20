37e2164254058bde36b2a08608f437edc3daf647
elixc20 <elicombs2020@gmail.com>
2024-02-08 15:42:52 -0600

    New Character Check Lab
    Test

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-#Remove pass and complete the code
 def check_character(word, index):
    if 0 < index < len(word):
       index_character = word[index]
@@ -16,3 +15,5 @@ if __name__ == '__main__':
     print(check_character('happy birthday', 5))
     print(check_character('happy birthday 2 you', 15))
     print(check_character('happy birthday!', 14))
+
+#Testing System
